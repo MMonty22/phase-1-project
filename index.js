280d7ec4f58b11234ddfc2458640760a398ec08a
Michael <michael.c.montgomery@gmail.com>
2022-04-15 15:20:26 -0500

    got data from api

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function getGames () {
+    fetch('http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1')
+    .then(res => res.json())
+    .then(games => {
+        for (let i=0; i <= games.dates[0].games.length; i++) {
+            //console.log('games', games)
+            renderGames(games, i)
+        }
+    })
+}
